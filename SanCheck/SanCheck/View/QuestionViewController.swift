17ff8978617b29d22497c1e1d0b30ee63d9071ea
yeahg-dev <yeahg.dev@gmail.com>
2022-09-14 14:12:19 +0900

    FEAT: QuestionView  ViewModel 바인딩 구현

--- SanCheck/SanCheck/View/QuestionViewController.swift
+++ SanCheck/SanCheck/View/QuestionViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    var viewModel: QuestionViewModel?
 
+    @IBOutlet weak var questionImageView: UIImageView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var option1Button: UIButton!
+    @IBOutlet weak var option2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        option1Button.configuration?.titleAlignment = .center
+        option2Button.configuration?.titleAlignment = .center
+        bind(viewModel: viewModel)
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func bind(viewModel: QuestionViewModel?) {
+        questionImageView.image = viewModel?.image
+        questionLabel.text = viewModel?.question
+        option1Button.titleLabel?.text = viewModel?.options[0]
+        option1Button.titleLabel?.text = viewModel?.options[1]
     }
-    */
 
 }
