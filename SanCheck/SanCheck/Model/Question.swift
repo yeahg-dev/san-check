5252bcadae8a3fc327f4449e1c7b929e3c6e8f3c
yeahg-dev <yeahg.dev@gmail.com>
2022-09-14 11:22:21 +0900

    FEAT: QuestionType, Question 구현

---
+++ SanCheck/SanCheck/Model/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  SanCheck
+//
+//  Created by Moon Yeji on 2022/09/14.
+//
+
+import UIKit
+
+struct Question {
+    
+    let question: String
+    let options: [String]
+    let image: UIImage?
+}
